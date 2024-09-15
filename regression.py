e93c6c36f9ffa9eb902d1f2bbbaa1224eef025df
Aman Ahmmed <aman.ahmmed2@mail.dcu.ie>
2024-09-15 17:21:26 +0100

    Added README.md (Not yet made), implemented model, training, and predictions

--- regression.py
+++ regression.py
@@ -7,12 +7,26 @@ from sklearn.utils import shuffle
 def linearRegression():
     # Read in data and trim
     data = pd.read_csv("data/student_mat_2173a47420.csv", sep=';')
-    data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
+    data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]] # G references grade period 1 = First period 2 = Second period 3 = Final
 
     # Prediction for later (Can be changed)
     predict = "G3"
 
     # Define training and test data
-    x = np.array(data.drop([predict], 1))
+    x = np.array(data.drop([predict], axis=1))
     y = np.array(data[predict])
-    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
+    trainingDataX, testDataX, trainingDataY, testDataY = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
+
+    # Establish Model
+    linear = linear_model.LinearRegression()
+
+    # Get line of best fit based on our training data
+    linear.fit(trainingDataX, trainingDataY)
+    accuracy = linear.score(testDataX, testDataY) # How accurate our line of best fit is
+
+    # Get predictions given our test data (testDataX)
+    predictions = linear.predict(testDataX)
+
+    # Compare our predictions to actual student results (testDataY)
+    for index, predictionValue in enumerate(predictions):
+        print(f'Input Data: {testDataX[index]}\nPredicted Result: {predictionValue:.2f}\nActual Result: {testDataY[index]}\n')
